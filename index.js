7d63252547f1ac6bb1ac6de54f511a66a063bb90
Piotr Kristowski <cybszot@wp.pl>
2020-06-28 13:11:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -68,20 +68,38 @@ document.addEventListener('keydown', function (event) {
 function moveBumps() {
     //16, 116, 216, 315, 415
 
-    console.log('Cones moved!');
-
+    //Get up-down position of all bumps
     var element = document.getElementById('bumps');
     var style = window.getComputedStyle(element)
-    let currentPosition = Number(style.getPropertyValue('margin-top').replace("px", ""));
+    let currentPositionDiagonal = Number(style.getPropertyValue('margin-top').replace("px", ""));
+    
+    //Get left-rigth position of each bump
+    var element1 = document.getElementById('bump1');
+    var style1 = window.getComputedStyle(element1)
+    let currentPositionHorizontal1 = Number(style1.getPropertyValue('padding-left').replace("px", ""));
 
-    console.log(currentPosition);
+    var element2 = document.getElementById('bump2');
+    var style2 = window.getComputedStyle(element2)
+    let currentPositionHorizontal2 = Number(style2.getPropertyValue('padding-left').replace("px", ""));
+
+    var element3 = document.getElementById('bump3');
+    var style3 = window.getComputedStyle(element3)
+    let currentPositionHorizontal3 = Number(style3.getPropertyValue('padding-left').replace("px", ""));
+
+    console.log(currentPositionHorizontal1, currentPositionHorizontal2, currentPositionHorizontal3);
+
+    console.log(currentPositionDiagonal);
     let newPosition = "";
 
-    if (currentPosition != 480) {
-        newPosition = currentPosition + 40;
+    if (currentPositionDiagonal <= 540) {
+        newPosition = currentPositionDiagonal + 40;
     }
+    else{
+        newPosition = 0;
+    }
+
     document.getElementById("bumps").style.marginTop = newPosition + "px";
 
 }
 
-setInterval(() => { moveBumps() }, 1000);
+setInterval(() => { moveBumps() }, 200);
