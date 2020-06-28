87d728b8280ac4b4914ba8111d7e22386159a323
Piotr Kristowski <cybszot@wp.pl>
2020-06-28 17:58:26 +0200

    update

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ var gameDelay = 10;
 
 document.addEventListener('keydown', function (event) {
     if (event.keyCode == 37) {
-        console.log('Left was pressed');
+      //  console.log('Left was pressed');
 
         var element = document.getElementById('racingCar')
         var style = window.getComputedStyle(element)
         let currentPosition = Number(style.getPropertyValue('padding-left').replace("px", ""));
 
-        console.log(currentPosition);
+      //  console.log(currentPosition);
         let newPosition = "";
         if (currentPosition != 400) {
             newPosition = currentPosition - 100;
@@ -17,13 +17,13 @@ document.addEventListener('keydown', function (event) {
         document.getElementById("racingCar").style.paddingLeft = newPosition + "px";
     }
     else if (event.keyCode == 39) {
-        console.log('Right was pressed');
+     //   console.log('Right was pressed');
 
         var element = document.getElementById('racingCar')
         var style = window.getComputedStyle(element)
         let currentPosition = Number(style.getPropertyValue('padding-left').replace("px", ""));
 
-        console.log(currentPosition);
+       // console.log(currentPosition);
         let newPosition = "";
         if (currentPosition != 800) {
             newPosition = currentPosition + 100;
@@ -33,13 +33,13 @@ document.addEventListener('keydown', function (event) {
     }
 
     else if (event.keyCode == 40) {
-        console.log('Down was pressed');
+     //   console.log('Down was pressed');
 
         var element = document.getElementById('racingCar')
         var style = window.getComputedStyle(element)
         let currentPosition = Number(style.getPropertyValue('margin-top').replace("px", ""));
 
-        console.log(currentPosition);
+      //  console.log(currentPosition);
         let newPosition = "";
 
         if (currentPosition != 480) {
@@ -50,13 +50,13 @@ document.addEventListener('keydown', function (event) {
     }
 
     else if (event.keyCode == 38) {
-        console.log('Up was pressed');
+      //  console.log('Up was pressed');
 
         var element = document.getElementById('racingCar')
         var style = window.getComputedStyle(element)
         let currentPosition = Number(style.getPropertyValue('margin-top').replace("px", ""));
 
-        console.log(currentPosition);
+      //  console.log(currentPosition);
         let newPosition = "";
         if (currentPosition != 0) {
             newPosition = currentPosition - 40;
@@ -79,10 +79,13 @@ function getRandomColor() {
 
 function gameOver() {
 
-    document.cookie = currentLevel;
+    var maxLevel = document.cookie;
+    console.log("Cookies: ", maxLevel)
     document.getElementById("gameOver").style.display = "block";
-    console.log("end!")
+    //console.log("end!")
     window.location.reload(true);
+
+    document.getElementById("maxLevel").value = maxLevel;
 
 }
 
@@ -114,8 +117,8 @@ function collisionHandler(buttonsPosition, button1, button2, button3) {
 
     var positions = [16, 116, 216, 315, 415];
 
-    console.log("Car position: ", currentCarPositionLeftRight, currentCarPositionUpDown);
-    console.log("Buttons position: ", buttonsPosition, button1, button2, button3);
+    // console.log("Car position: ", currentCarPositionLeftRight, currentCarPositionUpDown);
+    // console.log("Buttons position: ", buttonsPosition, button1, button2, button3);
 
     //Get lanes with bumps
     var currentBumpsPositions = [button1, button2, button3];
@@ -128,7 +131,7 @@ function collisionHandler(buttonsPosition, button1, button2, button3) {
                 }
             }
             else {
-                console.log("No collision");
+               // console.log("No collision");
             }
             //fullLanes.push(positionIndex);
         }
@@ -187,7 +190,7 @@ function moveBumps() {
     let currentPositionDiagonal = Number(style.getPropertyValue('margin-top').replace("px", ""));
 
     //Set up-down position of all bumps
-    console.log(currentPositionDiagonal);
+   // console.log(currentPositionDiagonal);
     let newPosition = "";
 
     //Get left-right position of each bump
@@ -203,7 +206,7 @@ function moveBumps() {
     var style3 = window.getComputedStyle(element3)
     let currentPositionHorizontal3 = Number(style3.getPropertyValue('padding-left').replace("px", ""));
 
-    console.log(currentPositionHorizontal1, currentPositionHorizontal2, currentPositionHorizontal3);
+   // console.log(currentPositionHorizontal1, currentPositionHorizontal2, currentPositionHorizontal3);
 
     if (currentPositionDiagonal <= 610) {
         newPosition = currentPositionDiagonal + scrollSpeed;
@@ -216,12 +219,13 @@ function moveBumps() {
         //Increse level
         var currentLevel = Number(document.getElementById('points').value);
 
-        console.log("current: ", currentLevel);
+      //  console.log("current: ", currentLevel);
         document.getElementById('points').value = currentLevel + 1;
+        document.cookie = currentLevel + 1;
 
         if (currentLevel % 5 == 0) {
             increaseDifficulty();
-            console.log("Increase difficulty!");
+          //  console.log("Increase difficulty!");
         }
 
         //Randomize bumps position
